62f1d4780f28d866e0e7a01d51ccdb98fd756d4e
hchowdhury3273 <hc2324@nyu.edu>
2019-08-21 21:16:52 -0400

    transport files to git repo

---
+++ InspireQuotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InspireQuotes
+//
+//  Created by Helal Chowdhury on 8/21/19.
+//  Copyright © 2019 Helal Chowdhury. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
